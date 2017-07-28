349f3c87cfd45b15f17595ce57ba01935104e70d
spectran <terentyev_ruslan@mail.ru>
2017-07-28 10:38:51 +0300

    Create rekordbox.h

---
+++ rekordbox.h
@@ -0,0 +1,20 @@
+/*
+ * rekordbox.h
+ *
+ *  Created on: 28 июля 2017 г.
+ *      Author: Spectran
+ */
+
+#ifndef REKORDBOX_H_
+#define REKORDBOX_H_
+
+typedef struct
+{
+	uint8_t *filename;
+	uint32_t lowp_spectrum_size;
+	uint32_t spectrum_size;
+}RekordboxTypeDef;
+
+void DecodeRekordboxFiles ();
+
+#endif /* REKORDBOX_H_ */
